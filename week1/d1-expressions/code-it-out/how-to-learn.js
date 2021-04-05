1e7949e6f598ac8adcd3deb630c636b735d8014a
Frema Awuku <frema.awuku@gmail.com>
2021-04-05 13:58:03 -0400

    frema notes

--- week1/d1-expressions/code-it-out/how-to-learn.js
+++ week1/d1-expressions/code-it-out/how-to-learn.js
@@ -3,16 +3,16 @@ What are the two 'types' of learning we alternate
 between here at AppAcademy? What are three examples 
 of each?
 
-1. 
-  1. 
+1. Active
+  1. exercise
   2. 
   3. 
-2. 
-  1.
-  2.
+2. Passive
+  1.reading material
+  2.watching a video
   3.
 
 What is Desirable Difficulty?
 
-
+we want what we are learning to have a desirded level of difficulty. (hard level on a video game)
 */
